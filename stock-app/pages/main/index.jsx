a387f13f68470377641cade4a4a5e8e31a8cbf4a
Mathias Agzigüzel Sørensen <110809330+Mathiassren@users.noreply.github.com>
2023-12-18 21:01:26 +0100

    test

--- stock-app/pages/main/index.jsx
+++ stock-app/pages/main/index.jsx
@@ -37,7 +37,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center relative min-h-screen">
       <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
-        <span className="text-primary tracking-widest">STOCKS</span>
+        <span className="text-primary text-pink-500 tracking-widest">
+          STOCKS
+        </span>
       </h2>
       <h3 className="text-primary text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
         Get info of any stocks quickly
